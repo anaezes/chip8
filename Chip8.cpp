c0e791a382200379d27ad35ab6eb7cd188264c9a
Ana Santos <anaezes@gmail.com>
2021-09-26 16:12:08 +0100

    Add more opcodes.

--- Chip8.cpp
+++ Chip8.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <sstream>
+#include <stdlib.h>
+#include <time.h>
 #include "Chip8.h"
 
 
@@ -20,7 +22,7 @@ Chip8::Chip8():
         sound_timer()
 {
     std::cout << "Hello World!" << std::endl;
-
+    srand (time(NULL));
     // Clear display
     // Clear registers V0-VF
 
@@ -74,9 +76,24 @@ void Chip8::emulateCycle() {
         {
             unsigned x = (opcode & 0x0F00) >> 8;
             unsigned char kk = opcode & 0x00FF;
-            if(registers[x] == kk)
-                pc += 2;
-            else pc += 4;
+            if(registers[x] == kk) pc += 4;
+            else pc += 2;
+        }
+            break;
+        case OPCODE_4XKK:
+        {
+            unsigned x = (opcode & 0x0F00) >> 8;
+            unsigned char kk = opcode & 0x00FF;
+            if(registers[x] != kk) pc += 4;
+            else pc += 2;
+        }
+            break;
+        case OPCODE_5XY0:
+        {
+            unsigned x = (opcode & 0x0F00) >> 8;
+            unsigned y = (opcode & 0x00F0) >> 4;
+            if(registers[x] == registers[y]) pc += 4;
+            else pc += 2;
         }
             break;
         case OPCODE_6XKK:
@@ -86,6 +103,42 @@ void Chip8::emulateCycle() {
             registers[x] = kk;
             pc += 2;
         }
+            break;
+        case OPCODE_7XKK:
+        {
+            unsigned x = (opcode & 0x0F00) >> 8;
+            unsigned char kk = opcode & 0x00FF;
+            registers[x] = registers[x] + kk;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8:
+            opcode8();
+            break;
+        case OPCODE_9XYK:
+        {
+            unsigned x = (opcode & 0x0F00) >> 8;
+            unsigned y = (opcode & 0x00F0) >> 4;
+            if(registers[x] != registers[y]) pc += 4;
+            else pc += 2;
+        }
+            break;
+        case OPCODE_BNNN:
+            pc = (opcode & 0x0FFF) + registers[0];
+            break;
+        case OPCODE_CXKK:
+        {
+            unsigned n = rand() % 256;
+            unsigned char kk = opcode & 0x00FF;
+            unsigned x = (opcode & 0x0F00) >> 8;
+            registers[x] = n & kk;
+            pc += 2;
+        }
+            break;
+        case OPCODE_DXYN:
+            // TODO: Display n-byte sprite starting at memory location I at (Vx, Vy),
+            //  set VF = collision.
+            pc += 2;
             break;
         case OPCODE_F:
             opcodeF();
@@ -112,12 +165,19 @@ void Chip8::emulateCycle() {
 
 void Chip8::opcodeF()
 {
-    switch(opcode & 0xF0FF)
+    switch(opcode & 0x00FF)
     {
         case OPCODE_FX15:
         {
             unsigned x = (opcode & 0x0F00) >> 8;
             delay_timer = registers[x];
+            pc += 2;
+        }
+            break;
+        case OPCODE_FX29:
+        {
+            unsigned x = (opcode & 0x0F00) >> 8;
+            index_register = registers[x] * 0x5;
             pc += 2;
         }
             break;
@@ -139,8 +199,90 @@ void Chip8::opcode0() {
             break;
 
         case OPCODE_RET:
+            --sp;
+            pc = stack[sp];
+            sp -= 1;
             break;
 
+        default:
+            std::stringstream ss;
+            ss << "unknown opcode: " << std::hex << opcode;
+            throw std::runtime_error(ss.str());
+    }
+}
+
+void Chip8::opcode8() {
+
+    unsigned x = (opcode & 0x0F00) >> 8;
+    unsigned y = (opcode & 0x00F0) >> 4;
+    unsigned char vx = registers[x];
+    unsigned char vy = registers[y];
+
+    switch(opcode & 0x000F)
+    {
+        case OPCODE_8XY0:
+        {
+            registers[x] = registers[y];
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY1:
+        {
+            registers[x] = vx | vy;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY2:
+        {
+            registers[x] = vx & vy;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY3:
+        {
+            registers[x] = vx ^ vy;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY4:
+        {
+            registers[x] = vx + vy;
+            if(vy + vx > 0xFF) registers[0xF] = 1;
+            else registers[0xF] = 0;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY5:
+        {
+            registers[x] = vx - vy;
+            if(vx > vy) registers[0xF] = 1;
+            else registers[0xF] = 0;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY6:
+        {
+           if(vx & 0x1 == 1) registers[0xF] = 1;
+           else registers[0xF] = 0;
+            registers[x] /= 2;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XY7:
+        {
+            registers[x] = vy - vx;
+            if(vy > vx) registers[0xF] = 1;
+            else registers[0xF] = 0;
+            pc += 2;
+        }
+            break;
+        case OPCODE_8XYE:
+        {
+            registers[0xF] = vx >> 7;
+            registers[x] *= 2;
+            pc += 2;
+        }
+            break;
         default:
             std::stringstream ss;
             ss << "unknown opcode: " << std::hex << opcode;
--- Chip8.h
+++ Chip8.h
@@ -26,17 +26,69 @@
 
 //! (1nnn) Jump to location nnn
 #define OPCODE_JP  0x1000
-//! Skip next instruction if Vx = kk.
+
+//! 0x3000 Skip next instruction if Vx = kk.
 //! The interpreter compares register Vx to kk,
 //! and if they are equal, increments the program counter by 2.
 #define OPCODE_3XKK 0x3000
-//! The interpreter puts the value kk into register Vx.
+
+//! 0x4000 Skip next instruction if Vx != kk. The interpreter compares
+//! register Vx to kk, and if they are not equal, increments the program counter by 2.
+#define OPCODE_4XKK 0x4000
+
+//! 0x5000 Skip next instruction if Vx = Vy. The interpreter compares register Vx
+//! to register Vy, and if they are equal, increments the program counter by 2.
+#define OPCODE_5XY0 0x5000
+
+//! 0x6000 The interpreter puts the value kk into register Vx.
 #define OPCODE_6XKK 0x6000
+
+//! 0x7000 Set Vx = Vx + kk.
+//! Adds the value kk to the value of register Vx, then stores the result in Vx.
+#define OPCODE_7XKK 0x7000
+
+//! 0x8 opcodes
+#define OPCODE_8 0x8000
+//! 0x8000 Set Vx = Vy. Stores the value of register Vy in register Vx.
+#define OPCODE_8XY0 0x0000
+//! 0x8001 Set Vx = Vx OR Vy.
+#define OPCODE_8XY1 0x0001
+//! 0x8002 Set Vx = Vx AND Vy.
+#define OPCODE_8XY2 0x0002
+//! 0x8003 Set Vx = Vx XOR Vy.
+#define OPCODE_8XY3 0x0003
+//! 0x8004 Set Vx = Vx + Vy, set VF = carry.
+#define OPCODE_8XY4 0x0004
+//! 0x8005 Set Vx = Vx - Vy, set VF = NOT borrow.
+#define OPCODE_8XY5 0x0005
+//! 0x8006 Set Vx = Vx SHR 1.
+#define OPCODE_8XY6 0x0006
+//! 0x8007 Set Vx = Vy - Vx, set VF = NOT borrow.
+#define OPCODE_8XY7 0x0007
+//! 0x800E Set Vx = Vx SHL 1.
+#define OPCODE_8XYE 0x000E
+
+//! 0x9000  Skip next instruction if Vx != Vy.
+#define OPCODE_9XYK 0x9000
+
+//! 0xB000  Jump to location nnn + V0.
+#define OPCODE_BNNN 0xB000
+
+//! 0xC000  Set Vx = random byte AND kk.
+#define OPCODE_CXKK 0xC000
+
+//! 0xD000 Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+#define OPCODE_DXYN 0xD000
 
 //! 0xF opcodes
 #define OPCODE_F 0xF000
 //! 0xF015 Set delay timer = Vx. DT is set equal to the value of Vx.
-#define OPCODE_FX15 0xF015
+#define OPCODE_FX15 0x0015
+//! 0xF029 Set I = location of sprite for digit Vx.
+//! The value of I is set to the location for the hexadecimal sprite corresponding
+//! to the value of Vx. See section 2.4, Display, for more information on the Chip-8
+//! hexadecimal font.
+#define OPCODE_FX29 0x0029
 
 
 class Chip8 {
@@ -81,6 +133,8 @@ public:
     void opcodeF();
 
     void opcode0();
+
+    void opcode8();
 };
 
 
